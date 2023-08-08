f4229f1b68e7eb2982672b64eb1584a3409bd522
CaioKN7 <galdinocaiost@gmail.com>
2023-08-08 17:37:09 -0300

    Exercicio 2 laços-i

---
+++ exercicio2/script.js
@@ -0,0 +1,8 @@
+//Exercício 2
+
+let numero = +prompt("Digite um número de 1 a 10:")
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i
+    console.log(`${numero}x${i}=${resultado}`)
+}
